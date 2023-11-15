5f781a8905288f3d0a05f2b08a67dff0604542ea
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-15 13:02:59 +0300

    methods checked in JS

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ button.addEventListener("click", result);
 function result(e) {
 e.preventDefault();
 
+let points = 0;
+
+if (document.querySelector("#answerOne").checked) {
+    points++   
+}
+
+if (document.querySelector("#answerTwo").checked) {
+    points++
+}
+
+if (document.querySelector("#answerThree").checked) {
+    points++
+}
+
+if (document.querySelector("#answerFour").checked) {
+    points++
+}
+
+if (document.querySelector("#answerFive").checked) {
+    points++
+}
 }
